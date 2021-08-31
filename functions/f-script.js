0434af38da19e26cf8f153086d3f68cdcd582774
kuzeva <kuzeva01@gmail.com>
2021-08-31 17:00:26 +0200

    challenge #1

--- functions/f-script.js
+++ functions/f-script.js
@@ -61,7 +61,6 @@ checkIn(flight,jonas)
 console.log(jonas);
 */
 
-
 // HIGHER-ORDER FUNCTIONS
 
 /* // Functions accepting Callback functions
@@ -213,4 +212,53 @@ const addTaxRate = function(rate){
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+*/
+
+/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// CHALLENGE #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+   //   console.log(this);
+     const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    typeof answer === 'number' &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+  
+// Bonus
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1]});
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+/*
+Test data for bonus:  
+Data 1: [5, 2, 3] 
+Data 2: [1, 5, 3, 9, 6, 1] 
 */
