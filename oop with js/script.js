ec90b5fad69dead761267bf81b532383b5cdb9a1
kuzeva <kuzeva01@gmail.com>
2021-09-10 17:07:25 +0200

    prototype intro

--- oop with js/script.js
+++ oop with js/script.js
@@ -29,3 +29,26 @@ const jack = new Person('Jack', 1998);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+
+// PROTOTYPES
+
+Person.prototype.calcAge = function () {
+   console.log(2031 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
