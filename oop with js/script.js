2c38d45a33572efba1fad59edf4adc60f42274bf
kuzeva <kuzeva01@gmail.com>
2021-09-13 14:38:02 +0200

    challenge

--- oop with js/script.js
+++ oop with js/script.js
@@ -331,3 +331,47 @@ ford.brake();
 ford.speedUS = 50;
 console.log(ford);
 */
+
+
+// CHALLENGE #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`'${this.make}' going at ${this.speed} km/h `);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`'${this.make}' going at ${this.speed} km/h `);
+};
+
+const EV = function (make, speed, charge) {
+   Car.call(this, make, speed);
+   this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  return this.charge = chargeTo;
+   // console.log(`Battery charged to ${this.charge}%`);
+}
+
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+
+EV.prototype.accelerate = function () {
+   this.speed += 20;
+   this.charge--;
+  console.log(`'${this.make}' going at ${this.speed} km/h, with a charge od ${this.charge}%`);
+}
+console.log(tesla);
+tesla.accelerate();
+tesla.brake();
+
+console.log(tesla.chargeBattery(90));
+tesla.accelerate();
