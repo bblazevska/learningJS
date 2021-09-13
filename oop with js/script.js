133d65083871dcad5bfc00ab2832783c0c9089b8
kuzeva <kuzeva01@gmail.com>
2021-09-13 11:28:09 +0200

    prototypal inheritance on built-in objects

--- oop with js/script.js
+++ oop with js/script.js
@@ -52,3 +52,26 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+//Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+
+const arr = [6, 5, 3, 9, 5, 7, 8, 9, 2, 3];
+
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+Array.prototype.unique = function () {
+   return [...new Set(this)];
+}
+console.log(arr);
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+console.dir( x=> x+1);
