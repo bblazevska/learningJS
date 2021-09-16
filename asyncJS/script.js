5f75d14f6b82dbf0809632c36f7bbe7d775f3eac
kuzeva <kuzeva01@gmail.com>
2021-09-16 12:33:21 +0200

    challenge #1

--- asyncJS/script.js
+++ asyncJS/script.js
@@ -195,7 +195,45 @@ const getCountryData = function (country) {
     });
 };
 
-btn.addEventListener('click', function () {
-  getCountryData('macedonia');
-});
-getCountryData('australia');
+// btn.addEventListener('click', function () {
+//   getCountryData('macedonia');
+// });
+// getCountryData('australia');
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// CHALLENGES 
+
+// #1
+
+const whereAmI = function (lat, lng) {
+   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+      .then(response => {
+         if (!response.ok)
+            throw new Error(`Problem with geocoding (${response.status})`);
+         return response.json();
+      })
+      .then(data => {
+         console.log(data);
+         console.log(`You are in ${data.city}, ${data.country}!`);
+        
+         return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+      })
+      .then(response => {
+         if (!response.ok)
+            throw new Error(`Country not found!(${response.status})`)
+         return response.json()
+      })
+      .then(data => renderCountry(data[0]))
+      .catch(err => console.error(`${err.message}`))
+      .finally(
+         countriesContainer.style.opacity = 1
+      );
+}
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
