87ee6ca0b551b7d77fa28ba91a119bba85576fd9
kuzeva <kuzeva01@gmail.com>
2021-09-16 11:25:55 +0200

    handling rejected promises

--- asyncJS/script.js
+++ asyncJS/script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+//   countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+   <article class="country ${className}">
+   <img class="country__img" src="${data.flag}" />
+   <div class="country__data">
+   <h3 class="country__name">${data.name}</h3>
+   <h4 class="country__region">${data.region}</h4>
+   <p class="country__row"><span>👫</span>${(
+     +data.population / 1000000
+   ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+      </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+};
+
+
 //////////////////////////////////////////////
 
 /*
@@ -38,25 +62,7 @@ getCountryData('macedonia');
 getCountryData('germany');
 getCountryData('italy');
 */
-const renderCountry = function (data, className = '') {
-   const html = `
-   <article class="country ${className}">
-   <img class="country__img" src="${data.flag}" />
-   <div class="country__data">
-   <h3 class="country__name">${data.name}</h3>
-   <h4 class="country__region">${data.region}</h4>
-   <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${
-         data.currencies[0].name
-      }</p>
-      </div>
-      </article>`;
-      countriesContainer.insertAdjacentHTML('beforeend', html);
-      countriesContainer.style.opacity = 1;
-   }
+
    
 /*
 
@@ -123,8 +129,6 @@ request.send();
 
 //Modern way= with fetch
 
-const request = fetch(`https://restcountries.eu/rest/v2/name/macedonia`);
-// console.log(request);
 
 const getCountryData = function (country) {
    //Country 1
@@ -138,8 +142,17 @@ const getCountryData = function (country) {
         return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
       })
       .then(response => response.json())
-      .then(data => renderCountry(data,'neighbour'));
+      .then(data => renderCountry(data, 'neighbour'))
+     .catch(err => {
+        console.log(`${err} 🎇🎇🎇`);
+        renderError(`Something went wrong 🎇🎇 ${err.message}`);
+     })
+     .finally(() => {
+         countriesContainer.style.opacity = 1;
+     });
 };
 
-getCountryData('macedonia');
+btn.addEventListener('click', function () {
+   getCountryData('macedonia');
+});
 // getCountryData('germany');
