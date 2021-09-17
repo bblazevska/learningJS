167c43de46d97d1f97bf2fc77823102ca81a0429
kuzeva <kuzeva01@gmail.com>
2021-09-17 12:55:35 +0200

    challenge #3

--- asyncJS/script.js
+++ asyncJS/script.js
@@ -384,7 +384,7 @@ Promise.reject(new Error('Problem!')).catch(x => console.error(x));
 
 /////////////////////////////////////
 // Running promises in parallel
-
+/*
 //Promise.all
 const get3Countries = async function (c1, c2, c3) {
   try {
@@ -449,12 +449,11 @@ Promise.any([
   Promise.reject('Error'),
   Promise.resolve('Another success'),
 ]).then(res => console.log(res));
-
-
-
+*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// CHALLENGES
 /*
+///////////////////////////////////////
 // #1
 
 const whereAmI = function (lat, lng) {
@@ -487,6 +486,7 @@ whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 */
 
+////////////////////////////////////////////
 // #2
 /*
 const imgContainer = document.querySelector('.images');
@@ -527,3 +527,59 @@ createImage('./img/img-1.jpg')
   .then(() => currentImg.style.display = 'none')
   .catch(err => console.error(err));
 */
+
+//////////////////////
+// #4
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found!'));
+    });
+  });
+};
+
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+    
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+    
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+    
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+
+    imgsEl.forEach(img => img.classList.add('parallel'))
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
